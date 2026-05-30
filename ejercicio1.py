3c1f31174d66ca0fdde1f72258bd36a009e2fa87
LEIDY2695 <leidyj2695@gmail.com>
2026-05-30 16:03:51 -0500

    ejercicio 2.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -6,6 +6,12 @@ for i in range(4):
     herraduras.append(colores)
 
 #comparacion de colores 
+comprar_herradura = 0
+
 if herraduras[0] == herraduras[1] or herraduras[0] == herraduras[2] or herraduras[0] == herraduras[3]:
-    print("SI")
-    
+    comprar_herradura = 1
+if herraduras[1] == herraduras[2] or herraduras[1] == herraduras[3]:
+    comprar_herradura = 1
+if herraduras[2] == herraduras[3]:
+    comprar_herradura = 1
+   print(comprar_herradura)
