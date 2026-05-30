5e293039ec26a8b71bcc88bfde1839b611e0026a
lauramaru00 <laumaru2006@gmail.com>
2026-05-30 16:32:13 -0500

    Actualizar partes del algoritmos y añadir comentarios del problema y solucion del algoritmo

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,11 @@
+# Problematica : Un cabello tiene 4 herraduras del año pasado pero no sabe cuantas herraduras son del mismo color.
+# El quiere reemplazarlas para que cada una quede de un color diferente a la moda actual. 
+# Y necesita realizar una compra de herraduras nuevas contando con el dinero suficiente,
+# sin embargo requiere ahorrar para gastar lo menos posible. 
+
+# Solucion : Desarrollar un sistema que permita identificar 
+# cuantas herraduras son del mismo color y asi determinar cuantas herraduras nuevas debe comprar el caballo.
+ 
 herraduras= []
 
 #Solicitud de colores de herraduras
@@ -9,9 +17,9 @@ for i in range(4):
 comprar_herradura = 0
 
 if herraduras[0] == herraduras[1] or herraduras[0] == herraduras[2] or herraduras[0] == herraduras[3]:
-    comprar_herradura = 1
+    comprar_herradura += 1
 if herraduras[1] == herraduras[2] or herraduras[1] == herraduras[3]:
-    comprar_herradura = 1
+    comprar_herradura += 1
 if herraduras[2] == herraduras[3]:
-    comprar_herradura = 1
-   print(comprar_herradura)
+    comprar_herradura += 1
+print(comprar_herradura)
